058b3fec88bbb8e47e3f88e179e691f084f7e20d
allfunso <74326155+allfunso@users.noreply.github.com>
2023-10-18 17:11:38 -0600

    Add files via upload

--- SEIRS_ml.jl
+++ SEIRS_ml.jl
@@ -1,51 +1,53 @@
 using Plots
-using Random
 using JLD2
+using Random
 
 #=
 Este código se emplea para generar una base de datos que sirva para entrenar una red neuronal
 Por lo tanto, se realiza la simulación varias veces y el vector de infectados se guarda en un archivo
 Normalizar la población y poner mismas condiciones iniciales nos ahorra parte del preprocesamiento, 
 pero es algo que se debe considerar en la aplicación para que el modelo sea efectivo en predicciones.
-Se toman los primeros 30 días y en base a eso decide qué tan grande será el pico.
+Se toman los primeros 40 días y en base a eso decide qué tan grande será el pico.
 =#
 
-prediction_span = 30
+prediction_span = 40 # Número de días que se proporciona al modelo
 
-#Valores iniciales de la poblacion (normalizado)
-N = 1000; #Numero total de la poblacion
+# Datos iniciales de la poblacion
+
+N = 10000; # Número total de la población
+R0 = 0; # Recuperados
+I0 = 10; # Infectados
+E0 = 0; # Expuestos
+S0 = N - I0; # Susceptibles
+
+# Tiempo de simulación
+
+tf = 200; # Días totales de simulacion
+dt = 1; # Delta del tiempo
+t = 0:dt:tf;
+iterations = length(t); #Numero de pasos para la simulacion
+
+#######################
 
 # Se repite toda la simulación para obtener una gran cantidad de datos
 
-R0 = 0/N; #Recuperados
-I0 = 1/N; #Infectados
-E0 = 0/N; #Expuestos
-S0 = 1 - I0; #Susceptibles
-
-#Valores iniciales para el tiempo 
-
-D = 100; #Dias totales de simulacion
-dt = 0.1; #Delta del tiempo tomado
-t = 0:dt:D;
-span = length(t); #Numero de pasos para la simulacion
-
-shots = 100
-I_data = zeros(shots, prediction_span+1)
+shots = 500
+I_data = zeros(prediction_span + 2, shots)
 
 for shot = 1:shots
 
+    # Parámetros beta, sigma, gamma y mu
 
-    # Probabilidades iniciales y constantes beta, sigma, gamma y mu
-    beta = 1; # Tasa de transmision
-    sigma = 1/7; # Tiempo de incubacion
-    gamma = 1/7; # Tasa de recuperacion
-    mu = 1/120 # Tasa de pérdida de inmunidad
+    beta = randn()*0.3^2 + 0.3; # Tasa de transmisión
+    sigma = randn()*0.4^2 + 0.4; # Tiempo de incubación
+    gamma = randn()*0.1^2 + 0.1; # Tasa de recuperación
+    mu = 1/365; # Tasa de pérdida de inmunidad
 
-    #Generar las matrices de las poblaciones
-    S = zeros(span);
-    E = zeros(span);
-    I = zeros(span);
-    R = zeros(span);
+    # Generar los vectores de las poblaciones
+    S = zeros(iterations);
+    E = zeros(iterations);
+    I = zeros(iterations);
+    R = zeros(iterations);
 
     S[1] = S0;
     E[1] = E0;
@@ -56,51 +58,39 @@ for shot = 1:shots
 
     # Generar el proceso de markov
 
-    for i = 2:span
-
-        # Probabilidades en funcion de la cantidad de personas por
-        # categoria (2do caso) considerando poblacion normalizada
-        pe = dt*beta*S[i-1]*I[i-1];
-        pi = dt*sigma*E[i-1];
-        pr = dt*gamma*I[i-1];
-        ps = dt*mu*R[i-1];
+    for i = 2:iterations
+        # Probabilidades
+        pSE = dt*beta * I[i-1]/N;
+        pEI = dt*sigma;
+        pIR = dt*gamma;
+        pRS = dt*mu;
 
         # Generar numeros aleatorios
-        Rns = rand(Int(round(S[i-1]*N)));
-        Rne = rand(Int(round(E[i-1]*N)));
-        Rni = rand(Int(round(I[i-1]*N)));
-        Rnr = rand(Int(round(I[i-1]*N)));
+        RnS = rand(Int(S[i-1]));
+        RnE = rand(Int(E[i-1]));
+        RnI = rand(Int(I[i-1]));
+        RnR = rand(Int(R[i-1]));
 
-        # Conseguir el numero de personas que cumplen con 
-        # las probabilidades establecidas (normalizado)
-        dS = sum(pe .> Rns)/N;
-        dE = sum(pi .> Rne)/N;
-        dI = sum(pr .> Rni)/N;
-        dR = sum(ps .> Rnr)/N;
+        # Conseguir el numero de personas que cumplen con las probabilidades establecidas
+        dS = sum(pSE .> RnS);
+        dE = sum(pEI .> RnE);
+        dI = sum(pIR .> RnI);
+        dR = sum(pRS .> RnR);
 
         #Obtener los nuevos valores de la poblacion
         S[i] = S[i-1] - dS + dR;
-        E[i] = E[i-1] + dS - dE;
-        I[i] = I[i-1] + dE - dI;
-        R[i] = R[i-1] + dI - dR;
+        E[i] = E[i-1] - dE + dS;
+        I[i] = I[i-1] - dI + dE;
+        R[i] = R[i-1] - dR + dI;
     end
-
+    # Extracción de datos y preprocesamiento (normalización)
+    start_day = Int(round(rand()*14) + 1);
+    end_day = start_day + prediction_span - 1;
+    I_norm = I ./ N;
+    I_max, idx = findmax(I_norm);
+    days_to_max = idx - end_day;
+    I_data[:, shot] = vcat(I_norm[start_day:end_day], [I_max, days_to_max]);
 end
 
-# Tiempo final en la grafica
-tf = Int(round(span / 2));
-
-# Graficacion
-plot(t[1:tf],S[1:tf]*100, label = "Poblacion susceptible", legend=:right)
-plot!(t[1:tf],E[1:tf]*100, label = "Poblacion expuesta")
-plot!(t[1:tf],I[1:tf]*100, label = "Poblacion infectada")
-plot!(t[1:tf],R[1:tf]*100, label = "Poblacion Recuperada")
-
-#Propiedades de la grafica
-title!("Modelo SEIRS (Poblacion normalizada)")
-
-xlabel!("Tiempo (dias)")
-ylabel!("% de la Poblacion Total")
-
-# Guardar la matriz para datos de entrenamiento
+# Guardar la matriz para datos de entrenamiento y prueba
 save_object("Infec_dataset.jld2", I_data)
