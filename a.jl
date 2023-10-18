ec0d548d19085a12adacd4f2c68e00dcac0fb18f
allfunso <74326155+allfunso@users.noreply.github.com>
2023-10-18 17:11:18 -0600

    Add files via upload

--- a.jl
+++ a.jl
@@ -1,25 +1,63 @@
-using Flux
-using Flux: @epochs
-using Base.Iterators: repeated
 using JLD2
+using Flux
+using Flux: train!
+using LinearAlgebra
+using Statistics
 
-I_data = load_object("Infec_dataset.jld2")
+I_data = Float32.(load_object("Infec_dataset.jld2"))
 
-# Define the architecture of the neural network
+x_train = Flux.flatten(I_data[1:40, 1:250])
+y_train = (I_data[41:42, 1:250])
+x_test = Flux.flatten(I_data[1:40, 251:500])
+y_test = I_data[41:42, 251:500]
+
+#= Define a simple convolutional neural network
 model = Chain(
-    # Reshape the input to (1, 100, 1) where (channels, height, width)
-    x -> reshape(x, 1, 100, 1),
-    Conv((3, 1) => 16, relu),
-    MaxPool((2, 1)),
-    Conv((3, 1) => 32, relu),
-    MaxPool((2, 1)),
-    x -> reshape(x, :, size(x, 4)),  # Flatten for fully connected layers
-    Dense(32, 10, relu),
-    Dense(10, 1)  # Output layer with 1 neuron
+    Conv((3, 1), 40=>64, relu),
+    x -> reshape(x, :, size(x, 4)),  # Flatten the output
+    Dense(64 * 38, 128, relu),
+    Dense(128, 2)  # Output layer with 2 units
+) =#
+model = Chain(
+    Dense(40, 20, relu),
+    Dense(20, 2)  # Output layer with 2 units
 )
 
-# Generate a random input vector of 100 elements
-input_vector = rand(100)
+# Define a sample input
+input = rand(40)
 
-# Pass the input through the model to get the output
-output = model(input_vector)
+# Forward pass through the network
+#output = model(input)
+#println(output)
+
+# Define loss function
+loss(x, y) = Flux.mse(model(x), y)
+
+# Track parameters
+ps = Flux.params(model)
+
+# Select optimizer
+learning_rate = 1
+opt = ADAM(learning_rate)
+
+# Train model
+loss_history = []
+epochs = 1000
+
+for epoch in 1:epochs
+    # Train model
+    train!(loss, ps, [(x_train, y_train)], opt)
+    # Print report
+    train_loss = loss(x_train, y_train)
+    push!(loss_history, train_loss)
+    println("Epoch = $epoch : Training Loss = $train_loss")
+end
+
+y_pred = model(x_test)
+
+mean(y_test - y_pred)
+
+index = collect(1:250)
+check_display = [index y_pred[1, :] y_pred[2, :] y_test[1, :] y_test[2, :]]
+
+vscodedisplay(check_display)
